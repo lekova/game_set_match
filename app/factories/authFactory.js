7fec5469ac7ae1549760d564b8951258ba955a09
lekova <lina.lekova@gmail.com>
2015-09-09 11:37:12 -0400

    Create authFactory in app/factories

---
+++ app/factories/authFactory.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function(){
+
+  angular
+  .module('gameSetMatch')
+  .factory('AuthFactory', AuthFactory);
+
+  AuthFactory.$inject = ['$http', '$location', '$window', 'appSettings'];
+
+   function AuthFactory($http, $location, $window, appSettings) {
+    var currentUser = {};
+
+    function getProfile() {
+      return $http.get(appSettings.apiUrl + '/refresh-navbar')
+        .then(function(response) {
+          angular.copy(response.data.user, currentUser);
+
+      });
+    };
+
+    var login = function(credentials){
+      return $http.post(appSettings.apiUrl + '/login', credentials).success(function(response){
+        angular.copy(response.user, currentUser);
+        simpleStorage.set('gl-user-token', response.token);
+        $http.defaults.headers.common.Authorization = 'Token token=' + response.token;
+        $location.path('');
+      });
+    };
+
+    var logOut = function(){
+      simpleStorage.flush();
+      $location.path('/try-glitchly');
+    };
+
+     var isLoggedIn = function(){
+      return simpleStorage.get('gl-user-token');
+    };
+
+    return {
+      login: login,
+      logOut: logOut,
+      getProfile: getProfile,
+      currentUser: currentUser,
+      isLoggedIn: isLoggedIn
+    };
+
+  };
+
+})();
+
