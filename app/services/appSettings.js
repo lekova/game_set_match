94fa40384ef3ac5895466989860a58bbcdd29d46
lekova <lina.lekova@gmail.com>
2015-09-08 22:49:58 -0400

    Add app settings in app/services

---
+++ app/services/appSettings.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function(){
+
+  angular.module("gameSetMatch").value('appSettings', {
+    apiUrl: "http://localhost:3000",
+    title: "Game Set Match",
+    version: "1.0"
+  });
+
+})();
+
