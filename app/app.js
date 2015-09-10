ff77a94e4dabd1edeb18a086df37e6c6bce796e0
lekova <lina.lekova@gmail.com>
2015-09-10 13:50:56 -0400

    Change routes for when user is not authenticated in app/app.js

--- app/app.js
+++ app/app.js
@@ -1,22 +1,17 @@
 'use strict';
 
-angular
-  .module('gameSetMatch', [
-    'ngRoute',
-    'MainController',
-    'MainDirective'
-  ])
-  .config(function ($httpProvider) {
-    $httpProvider.interceptors.push('fourOhOneInterceptor');
-  })
-  .run(function($rootScope, $http, $window, $location, $routeParams, AuthFactory, UserFactory, PictureFactory) {
+angular.module('gameSetMatch', ['ngRoute'])
+  // .config(function ($httpProvider) {
+  //   $httpProvider.interceptors.push('fourOhOneInterceptor');
+  // })
+  .run(function($rootScope, $http, $window, $location, $routeParams, AuthFactory, UserFactory) {
 
     if(AuthFactory.isLoggedIn()){
       var data = simpleStorage.get('gl-user-token');
       $http.defaults.headers.common.Authorization = 'Token token=' + data;
     }
 
-    var routesThatDontRequireAuth = ['/login', '/', '/sign-up'];
+    var routesThatDontRequireAuth = ['/login', '/home', '/signup'];
 
     var routeClean = function (route) {
       return (routesThatDontRequireAuth.indexOf(route) > -1);
@@ -26,7 +21,7 @@ angular
 
       if (!routeClean($location.url()) && !AuthFactory.isLoggedIn()) {
         // redirect back to login
-        $location.path('/');
+        $location.path('/home');
       }
 
       // if ($location.path() === '/users') {
@@ -36,7 +31,6 @@ angular
       // } else if ($location.path() === '/following') {
       //   UserFactory.getFollowing();
       // }
-
     });
 
   });
