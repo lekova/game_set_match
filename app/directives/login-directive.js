6b1f63e3d719d554354cdfcc74e8e69211e9b30b
lekova <lina.lekova@gmail.com>
2015-09-10 14:10:21 -0400

    Create login directive

---
+++ app/directives/login-directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function(){
+
+  angular.module('gameSetMatch').directive('glLoginForm', glLoginForm);
+
+  function glLoginForm(){
+
+    return {
+      restrict: 'E',
+      templateUrl: 'app/views/login-form.html',
+      controller: 'AuthCtrl',
+      controllerAs: 'AuthCtrl',
+      bindToController: true,
+      scope: {
+        credentials: '&'
+      }
+    };
+  };
+
+})();
