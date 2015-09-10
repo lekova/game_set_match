9d5cffe138bbaf0f8dfeb5df57a43c61fcfacd4d
lekova <lina.lekova@gmail.com>
2015-09-10 14:10:59 -0400

    Create signup directive

---
+++ app/directives/signup-directive.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function(){
+
+  angular.module('gameSetMatch').directive('glSignUpForm', glSignUpForm);
+
+
+  function glSignUpForm() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/views/signup-form.html',
+      controller: 'UserCtrl',
+      controllerAs: 'UserCtrl',
+      bindToController: true
+    };
+  };
+
+})();
