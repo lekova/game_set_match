8d982c737280592d0044e741275d98b7f070d379
lekova <lina.lekova@gmail.com>
2015-09-10 22:18:48 -0400

    Unsignificant changes in multiple files

---
+++ app/scripts/map.js
@@ -0,0 +1,11 @@
+// var map;
+// function initMap() {
+//   console.log("Hello map");
+//   console.log("document.getElementById('map'):", document.getElementById('map'));
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -42.356, lng: -71.061},
+//     zoom: 8
+//   });
+// };
+
+// initMap();
