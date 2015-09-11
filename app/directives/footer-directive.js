67a443be8f0252cb5ba40dbdc986c2be1a3ce772
lekova <lina.lekova@gmail.com>
2015-09-10 21:48:08 -0400

    Create footer directive

---
+++ app/directives/footer-directive.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function(){
+
+  angular.module('gameSetMatch').directive('glFooter', glFooter);
+
+  function glFooter(){
+
+    return {
+      restrict: 'E',
+      templateUrl: 'app/views/footer.html'
+    };
+  };
+
+})();
