4335ea5c0b9f8d229bbdaafab1bab2742d28b34a
lekova <lina.lekova@gmail.com>
2015-09-17 17:04:57 -0400

    Create gameFactory and game Controller and link the files in index.html

---
+++ app/controllers/gamesController.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function(){
+
+  angular.module('gameSetMatch').controller('GamesCtrl', GamesCtrl);
+
+  GamesCtrl.$inject = ['$location', 'GameFactory', 'AuthFactory', '$scope'];
+
+  function GamesCtrl($location, GameFactory, AuthFactory, $scope){
+    var vm = this;
+    vm.currentUser = AuthFactory.currentUser;
+    vm.wonGames = GameFactory.wonGames;
+    vm.lostGames = GameFactory.lostGames;
+    vm.upcomingGames = GameFactory.upcomingGames;
+    vm.game = {};
+
+    vm.getWonPastGames = function() {
+      GameFactory.getWonPastGames();
+    }
+
+    vm.getLostPastGames = function() {
+      GameFactory.getLostPastGames();
+    }
+
+    vm.getUpcomingGames = function() {
+      GameFactory.getUpcomingGames();
+    }
+
+    vm.getWonPastGames();
+    vm.getLostPastGames();
+    vm.getUpcomingGames();
+  };
+
+})();
