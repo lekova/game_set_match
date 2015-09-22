3560dc0090b760468b7be8476dd98e76bb9afd8e
lekova <lina.lekova@gmail.com>
2015-09-22 07:16:24 -0400

    Add chartController.js template to set up dashboard chart

---
+++ app/controllers/chartController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function() {
+	angular.module('gameSetMatch').controller('ChartsCtrl', ChartsCtrl);
+
+	ChartsCtrl.$inject = ['$location', 'ChartFactory', 'GameFactory', '$scope'];
+
+	function ChartCtrl($location, ChartFactory, GameFactory, $scope) {
+      var vm = this;
+
+    };
+
+})();
