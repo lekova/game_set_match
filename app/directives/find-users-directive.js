a7e1b05dd6d674e2ffbdd0f820bc9ad0f14ce3a6
lekova <lina.lekova@gmail.com>
2015-09-29 17:46:48 -0400

    Remove controller binding in find-users-directive.
    Double binding is causing the Controller to be called twice making twice as much calls to the database.

--- app/directives/find-users-directive.js
+++ app/directives/find-users-directive.js
@@ -9,9 +9,6 @@
     return {
       restrict: 'E',
       templateUrl: 'app/views/find-users-page.html',
-      controller: 'UserCtrl',
-      controllerAs: 'UserCtrl',
-      bindToController: true,
       link: function(scope) {
         scope.UserCtrl.findUsers();
       }
