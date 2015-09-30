9f1193d7c2d31f8e6af74dc36ce62d0b67a91f1a
lekova <lina.lekova@gmail.com>
2015-09-30 03:08:38 -0400

    Formatting from Atom linter

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -2,104 +2,109 @@
 
 (function() {
 
-		angular.module('gameSetMatch').controller('UserCtrl', UserCtrl);
+	angular.module('gameSetMatch').controller('UserCtrl', UserCtrl);
 
-		UserCtrl.$inject = ['$routeParams','UserFactory', 'AuthFactory'];
+	UserCtrl.$inject = ['$routeParams','UserFactory', 'AuthFactory'];
 
-    function UserCtrl($routeParams, UserFactory, AuthFactory) {
-        var vm = this;
-        var userId = $routeParams.userId;
-        vm.users = UserFactory.users;
-        vm.user = UserFactory.user;
-        vm.search = UserFactory.search;
-        vm.editMode = false;
-        vm.credentials = {};
-        vm.userProciencyType = {};
-        vm.currentUser = AuthFactory.currentUser;
+	function UserCtrl($routeParams, UserFactory, AuthFactory) {
+		var vm = this;
+		var userId = $routeParams.userId;
+		vm.users = UserFactory.users;
+		vm.user = UserFactory.user;
+		vm.search = UserFactory.search;
+		vm.editMode = false;
+		vm.credentials = {};
+		vm.userProficiencyType = {};
+		vm.userAddress = {};
+		vm.currentUser = AuthFactory.currentUser;
 		vm.opponents = UserFactory.opponents;
 
-        vm.createUser = function() {
-            UserFactory.createUser({credentials: vm.user, proficiency: vm.userProficiencyType}).then(function(response) {
-                vm.credentials = {};
-                $location.path('');
-            }, function(response) {
-                vm.serverErrors = true;
-            });
-        };
+		vm.createUser = function() {
+			UserFactory.createUser({
+					credentials: vm.user,
+					proficiency: vm.userProficiencyType,
+					userAddress: vm.userAddress
+				}).then(function(response) {
+				vm.credentials = {};
+				$location.path('');
+			}, function(response) {
+				vm.serverErrors = true;
+			});
+		};
 
 		vm.getOpponents = function() {
 			UserFactory.getOpponents();
 		};
 
-        vm.closeWarningMessage = function() {
-            vm.serverErrors = !vm.serverErrors;
-        };
+		vm.closeWarningMessage = function() {
+			vm.serverErrors = !vm.serverErrors;
+		};
 
-        vm.updateUser = function(user) {
-            UserFactory.updateUser(user);
-        };
+		vm.updateUser = function(user) {
+			UserFactory.updateUser(user);
+		};
 
-        vm.showAllUsers = function() {
-            UserFactory.getUsers();
-        }
+		vm.showAllUsers = function() {
+			UserFactory.getUsers();
+		}
 
-        vm.showProfile = function() {
-          UserFactory.getProfile();
-        };
+		vm.showProfile = function() {
+		  UserFactory.getProfile();
+		};
 
-        vm.followUser = function(id) {
-            UserFactory.followUser(id);
-        };
+		vm.followUser = function(id) {
+			UserFactory.followUser(id);
+		};
 
-        vm.renderPreview = function() {
-            var reader = new FileReader();
+		vm.renderPreview = function() {
+			var reader = new FileReader();
 
-            reader.onload = function(e) {
-                $('#profile').attr('src', e.target.result);
-            };
+			reader.onload = function(e) {
+				$('#profile').attr('src', e.target.result);
+			};
 
-            reader.readAsDataURL($('#profile-preview')[0].files[0]);
-        };
+			reader.readAsDataURL($('#profile-preview')[0].files[0]);
+		};
 
-        vm.isCurrentUser = function(id) {
-            return (AuthFactory.currentUser.id === id);
-        };
+		vm.isCurrentUser = function(id) {
+			return (AuthFactory.currentUser.id === id);
+		};
 
-        vm.unfollowUser = function(id) {
-            UserFactory.unfollowUser(id);
-        };
+		vm.unfollowUser = function(id) {
+			UserFactory.unfollowUser(id);
+		};
 
-        vm.showUser = function() {
-          UserFactory.getUser(userId);
-        };
+		vm.showUser = function() {
+		  UserFactory.getUser(userId);
+		};
 
-        vm.searchUsers = function() {
-            UserFactory.getUsers(search);
-        };
+		vm.searchUsers = function() {
+			UserFactory.getUsers(search);
+		};
 
-        vm.removeUser = function(user) {
-            UserFactory.removeUser(user);
-        };
+		vm.removeUser = function(user) {
+			UserFactory.removeUser(user);
+		};
 
-        vm.findUsers = function() {
-            UserFactory.findUsers(vm.address);
-        }
+		vm.findUsers = function() {
+			UserFactory.findUsers(vm.address);
+		}
 
-        function resetForm() {
-            vm.user = {};
-            vm.serverErrors = false;
-        };
+		function resetForm() {
+			vm.user = {};
+			vm.serverErrors = false;
+		};
 
-        vm.cancel = function() {
-            resetForm();
-        };
+		vm.cancel = function() {
+			resetForm();
+		};
 
 		function init() {
 			vm.getOpponents();
-            vm.showAllUsers();
+			vm.showAllUsers();
 		}
 
 		init();
-    };
+	};
 
 })();
