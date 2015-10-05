4eee649d507bdb382c6b208fcfc600fcd15bb716
lekova <lina.lekova@gmail.com>
2015-10-04 22:32:09 -0400

    Create find players controller.
    It is to replace the usage of UsersController on Find Players page.

---
+++ app/controllers/findPlayersController.js
@@ -0,0 +1,25 @@
+'use strict';
+(function() {
+	angular.module('gameSetMatch').controller('FindPlayersCtrl', FindPlayersCtrl);
+	FindPlayersCtrl.$inject = ['AuthFactory', 'UserFactory', 'ProficiencyFactory'];
+
+	function FindPlayersCtrl(AuthFactory, UserFactory, ProficiencyFactory) {
+		var vm = this;
+
+		vm.currentUser = AuthFactory.currentUser;
+		vm.proficiencyTypes = ProficiencyFactory.proficiencyTypes;
+		vm.players = UserFactory.users;
+		vm.showPlayers = false;
+
+		vm.getProficiencyTypes = function() {
+			ProficiencyFactory.getProficiencyLevels();
+		}
+
+		vm.findPlayers = function() {
+			UserFactory.findUsers(vm.city);
+			vm.showPlayers = true;
+		}
+
+		vm.getProficiencyTypes();
+	}
+})();
