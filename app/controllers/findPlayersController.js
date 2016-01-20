a392997b32fc0399940ad3c6bc9ccaa2f96ff8c3
lekova <lina.lekova@gmail.com>
2016-01-20 15:21:54 -0500

    Update in find player feature after back-end api returns users and proficiency level together.

--- app/controllers/findPlayersController.js
+++ app/controllers/findPlayersController.js
@@ -1,26 +1,20 @@
 'use strict';
+
 (function() {
 	angular.module('gameSetMatch').controller('FindPlayersCtrl', FindPlayersCtrl);
 
-	FindPlayersCtrl.$inject = ['AuthFactory', 'UserFactory', 'ProficiencyFactory'];
+	FindPlayersCtrl.$inject = ['AuthFactory', 'UserFactory'];
 
-	function FindPlayersCtrl(AuthFactory, UserFactory, ProficiencyFactory) {
+	function FindPlayersCtrl(AuthFactory, UserFactory) {
 		var vm = this;
 
 		vm.currentUser = AuthFactory.currentUser;
-		vm.proficiencyTypes = ProficiencyFactory.proficiencyTypes;
 		vm.players = UserFactory.users;
 		vm.showPlayers = false;
-
-		vm.getProficiencyTypes = function() {
-			ProficiencyFactory.getProficiencyLevels();
-		};
 
 		vm.findPlayers = function() {
 			UserFactory.findUsers(vm.city);
 			vm.showPlayers = true;
 		};
-
-		vm.getProficiencyTypes();
 	}
 })();
