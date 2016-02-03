f57906e97d550f8995c4056ad42dea8712a511a7
lekova <lina.lekova@gmail.com>
2016-02-03 10:56:55 -0500

    Add map initialize and set bounds to find player controller. Change the find player function to load markers when player are returned from db.

--- app/controllers/findPlayersController.js
+++ app/controllers/findPlayersController.js
@@ -11,10 +11,64 @@
 		vm.currentUser = AuthFactory.currentUser;
 		vm.players = UserFactory.users;
 		vm.showPlayers = false;
+		vm.map = {};
 
 		vm.findPlayers = function() {
-			UserFactory.findPlayers(vm.city);
-			vm.showPlayers = true;
+			UserFactory.findPlayers(vm.city).then(function(response) {
+				console.log("hello players");
+				vm.players = response.data;
+				vm.showPlayers = true;
+				vm.setMapBounds();
+			}).catch(function(error) {
+				console.log("error is ", error);
+			});
+		};
+
+		vm.initialize = function() {
+			var myLatlng = new google.maps.LatLng(42.355, -71.067857);
+			var myOptions = {
+				zoom: 11,
+				center: myLatlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			vm.map = new google.maps.Map(document.getElementById("playersMap"), myOptions);
+		};
+
+		vm.initialize();
+
+		// set map bounds around the markers
+		vm.setMapBounds = function() {
+			var coordinates = [];
+			var arrayOfMarkers = [];
+			var bounds = new google.maps.LatLngBounds();
+
+			// get the coordinates from all the addresses of every player
+			vm.players.forEach(function(player) {
+				player.addresses.forEach(function(addr) {
+					coordinates.push([addr.lat, addr.lng]);
+				});
+			});
+
+			if (coordinates.length === 0) {
+				return;
+			}
+
+			// put all coordinates in an array
+			coordinates.forEach(function(item) {
+				var marker = new google.maps.Marker({
+					map: vm.map,
+					position: new google.maps.LatLng(item[0], item[1])
+				});
+				arrayOfMarkers.push(marker);
+			});
+
+			// get all coorcinated from the array and create bounds from them
+			// apply the bounds to the map
+			arrayOfMarkers.forEach(function(markerItem) {
+				bounds.extend(markerItem.getPosition());
+			});
+			vm.map.fitBounds(bounds);
 		};
 	}
 })();
