a2e700fd43ba7d9560d4bdfd09fe8fa896b0fde3
lekova <lina.lekova@gmail.com>
2016-02-12 10:47:47 -0500

    Remove initialize map funciton from address controller.

--- app/controllers/addressesController.js
+++ app/controllers/addressesController.js
@@ -30,19 +30,6 @@
 			AddressFactory.updateAddress(vm.selected);
 		};
 
-		vm.initialize = function() {
-			var myLatlng = new google.maps.LatLng(42.355, -71.067857);
-			var myOptions = {
-				zoom: 11,
-				center: myLatlng,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-
-			vm.map = new google.maps.Map(document.getElementById("userMap"), myOptions);
-		};
-
-		vm.initialize();
-
 		vm.codeAddress = function codeAddress(address) {
 			var addressStr = address.street + ",  " + address.city + ", " + address.state + ", " + address.zipcode;
 			console.log(addressStr);
@@ -51,7 +38,6 @@
 			return new Promise(function(resolve, reject) {
 				geocoder.geocode( { 'address': addressStr }, function(results, status) {
 					if (status == google.maps.GeocoderStatus.OK) {
-						position: results[0].geometry.location
 						address.lat = results[0].geometry.location.lat();
 						address.lng = results[0].geometry.location.lng();
 						resolve(address);
