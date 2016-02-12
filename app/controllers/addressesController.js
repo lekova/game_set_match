d7040409a4bb33e25dcdb190daa07b52d7ab98d6
lekova <lina.lekova@gmail.com>
2016-02-12 09:36:28 -0500

    Add geolocator funciton in addresses controller to obtain the lat in lng.

--- app/controllers/addressesController.js
+++ app/controllers/addressesController.js
@@ -6,22 +6,63 @@
 	AddressesCtrl.$inject = ['AuthFactory', 'AddressFactory'];
 
 	function AddressesCtrl(AuthFactory, AddressFactory) {
-		this.address = {};
-		this.selected = {};
-		this.addressTab = true;
-		this.currentUser = AuthFactory.currentUser;
+		var vm = this;
+		vm.address = {};
+		vm.selected = {};
+		vm.addressTab = true;
+		vm.currentUser = AuthFactory.currentUser;
 
-		this.createAddress = function() {
-			AddressFactory.createAddress(this.address);
+		vm.createAddress = function() {
+			var result = vm.codeAddress(vm.address);
+			result.then(function(value) {
+				AddressFactory.createAddress(value);
+			}).catch(function(error) {
+				// TODO create error message for the user
+				console.log("promise error is ", error);
+			});
 		};
 
-		this.toggleTab = function(bool) {
-			// this.addressTab = (this.addressTab === false) ?  true : false;
-			this.addressTab = bool;
+		vm.toggleTab = function(bool) {
+			// vm.addressTab = (vm.addressTab === false) ?  true : false;
+			vm.addressTab = bool;
 		};
 
-		this.updateAddress = function() {
-			AddressFactory.updateAddress(this.selected);
+		vm.updateAddress = function() {
+			AddressFactory.updateAddress(vm.selected);
 		};
-	};
+
+		vm.initialize = function() {
+			// TODO get users current location
+			var myLatlng = new google.maps.LatLng(42.355, -71.067857);
+			var myOptions = {
+				zoom: 11,
+				center: myLatlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			vm.map = new google.maps.Map(document.getElementById("userMap"), myOptions);
+		};
+
+		vm.initialize();
+
+		vm.codeAddress = function codeAddress(address) {
+			var addressStr = address.street + ",  " + address.city + ", " + address.state + ", " + address.zipcode;
+			console.log(addressStr);
+
+			var geocoder = new google.maps.Geocoder();
+			return new Promise(function(resolve, reject) {
+				geocoder.geocode( { 'address': addressStr }, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						position: results[0].geometry.location
+						address.lat = results[0].geometry.location.lat();
+						address.lng = results[0].geometry.location.lng();
+						resolve(address);
+					} else {
+						console.log("Geocode was not successful for the following reason: " + status);
+						reject(status);
+					}
+				});
+			});
+		};
+	}
 })();
